aa8b5b8e2e9b465071b8014e6adb1982df8f7e04
avndvd2409 <avndvd2409@gmail.com>
2021-10-06 19:28:17 +0530

    Canged the webpack confi

--- webpack.config.js
+++ webpack.config.js
@@ -19,26 +19,9 @@ module.exports = {
             {
                 test: /\.(sa|sc|c)ss$/,
                 use: [
-                    {
-                        loader: 'style-loader',
-                        options: {
-                            hmr: true
-                        }
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                            importLoaders: 1,
-                            localIdentName: '[name]__[local]___[hash:base64:5]',
-                        },
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: [autoprefixer({ browsers: ['> 1%'] })]
-                        },
-                    },
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader',
                     'sass-loader'
                 ],
             }
